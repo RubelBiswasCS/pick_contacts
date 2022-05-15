42be1700d4d4c913fe5f793f09b65e709764791f
RubelBiswasDH <RubelBiswasDH@gmail.com>
2022-05-15 23:58:55 +0600

    modified regex

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 # pick phone number form text file 
 
 import re
+regex = r"(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
+# regex = r"\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}"
 
 all_contacts = []
-lines = []
+m =[]
 
-for i in range(10):
+for i in range(3):
     all_phones = []
     with open(f'{i+1}.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            phones = re.findall(r"\d\d\d-\d\d\d-\d\d\d\d",line)
-            all_phones.extend(phones)
-
+        text = f.read()
+        phones = re.findall(regex, text)
+        
+        if phones: 
+            for phone in phones:
+                if len(phone) >= 10: # ignore number less than 10 digit
+                    all_phones.append(phone)
+            
     contacts = {
         'source': f'{i+1}.txt',
-        'contact_list' : all_phones
+        'contact_list' : all_phones,
+
     }
 
     all_contacts.append(contacts)
 
-
 for contacts in all_contacts:
     for key,value in contacts.items():
         print(f"{key} : {value}")
+
